e01c977ba2b4d7057a960c2be94fffcb149eba47
Analyst-FPnA <analyst.fpna@gmail.com>
2025-06-23 09:03:51 +0700

    Update version.py

--- version.py
+++ version.py
@@ -1,2 +1,19 @@
 version = "v2.0.1"
 data = "10/06/2025"
+success_html = """        
+<div style="
+    background-color: #d4edda; 
+    color: #155724; 
+    border-radius: 5px; 
+    font-size: 11px;
+    font-weight: 600;
+    border: 1px solid #c3e6cb;
+">
+<ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
+    <li style="margin-bottom: 6px;">Perubahan tampilan web</li>
+    <li style="margin-bottom: 6px;">Penambahan fitur pembaruan versi secara otomatis</li>
+    <li style="margin-bottom: 6px;">Penambahan halaman "Home"</li>
+    <li>Perbaikan bug pada modul "PENYESUAIAN IA" [SCM-Cleaning]</li>
+</ul>
+</div>
+"""
