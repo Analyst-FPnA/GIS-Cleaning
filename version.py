c2e0ab4695a63bbaff0716363c0d5435c1a84e08
Analyst-FPnA <analyst.fpna@gmail.com>
2025-06-23 09:42:35 +0700

    Update version.py

--- version.py
+++ version.py
@@ -11,7 +11,7 @@ detail = """
             padding: 10px 12px;
         ">
         <div style="font-size: 13px; font-weight: bold; margin-bottom: 8px;">
-            Latest Version Update Details: v2.0.1
+            New Version Update Details: v2.0.1
         </div>
         <ul style="padding-left: 18px; margin: 0;">
             <li style="margin-bottom: 6px;">Updated web interface for a cleaner and more modern look</li>
