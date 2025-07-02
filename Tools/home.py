4dde04509bceda3d3378c696aefba7ef11126272
Analyst-FPnA <analyst.fpna@gmail.com>
2025-07-02 15:31:08 +0700

    Update home.py

--- Tools/home.py
+++ Tools/home.py
@@ -18,7 +18,7 @@ col = st.columns([4,1])
 with col[0]:
     st.image(image)
 with col[1]:
-    with st.expander("Latest Version Update Details: v2.1.2"):
+    with st.expander("Latest Version Update Details: v2.1.3"):
 
         success_html = """
         <div style="
@@ -30,7 +30,7 @@ with col[1]:
             border: 1px solid #c3e6cb;
         ">
         <ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
-            <li>Error Fix for REKAP 42.02 module [SCM-Processing]</li>
+            <li>Column Format Adjustment on REKAP PENYESUAIAN INPUTAN IA module [SCM-Processing]</li>
         </ul>
         </div>
         """
