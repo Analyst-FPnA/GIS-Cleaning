839e1ed98ca2542c70c8bff95e5dc4cc81fa5553
Analyst-FPnA <analyst.fpna@gmail.com>
2025-08-22 08:28:25 +0700

    Update home.py

--- Tools/home.py
+++ Tools/home.py
@@ -18,7 +18,7 @@ col = st.columns([4,1])
 with col[0]:
     st.image(image)
 with col[1]:
-    with st.expander("Latest Version Update Details: v2.1.8"):
+    with st.expander("Latest Version Update Details: v2.1.9"):
 
         success_html = """
         <div style="
@@ -30,7 +30,7 @@ with col[1]:
             border: 1px solid #c3e6cb;
         ">
         <ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
-            <li>Fixing the Error on 42.15 module [GIS-Processing]</li>
+            <li>New Page [COM Monitoring]</li>
         </ul>
         </div>
         """
