3f3671070d084425b3ba16bdde5e296e91424bde
Analyst-FPnA <analyst.fpna@gmail.com>
2025-09-12 08:36:10 +0700

    Update home.py

--- Tools/home.py
+++ Tools/home.py
@@ -18,7 +18,7 @@ col = st.columns([4,1])
 with col[0]:
     st.image(image)
 with col[1]:
-    with st.expander("Latest Version Update Details: v2.1.10"):
+    with st.expander("Latest Version Update Details: v2.1.11"):
 
         success_html = """
         <div style="
@@ -30,7 +30,7 @@ with col[1]:
             border: 1px solid #c3e6cb;
         ">
         <ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
-            <li>Resolving the issue in the export feature of [COM Monitoring]</li>
+            <li>New item added to the REKAP DATA BOM-DEVIASI [SCM-Processing]</li>
         </ul>
         </div>
         """
