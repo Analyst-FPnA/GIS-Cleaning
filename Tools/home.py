96695efd28e4e1688ef15bb6cd8e997de36218cc
Analyst-FPnA <analyst.fpna@gmail.com>
2025-09-18 11:13:55 +0700

    Update home.py

--- Tools/home.py
+++ Tools/home.py
@@ -18,7 +18,7 @@ col = st.columns([4,1])
 with col[0]:
     st.image(image)
 with col[1]:
-    with st.expander("Latest Version Update Details: v2.1.11"):
+    with st.expander("Latest Version Update Details: v2.1.12"):
 
         success_html = """
         <div style="
@@ -30,7 +30,7 @@ with col[1]:
             border: 1px solid #c3e6cb;
         ">
         <ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
-            <li>New item added to the REKAP DATA BOM-DEVIASI [SCM-Processing]</li>
+            <li>"Insert 3224 to the 4205 column [COM-Monitoring]"</li>
         </ul>
         </div>
         """
