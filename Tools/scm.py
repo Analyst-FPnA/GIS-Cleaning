388b67b6aba79ebed1db12d526ba18757e289b62
Analyst-FPnA <analyst.fpna@gmail.com>
2026-02-18 09:44:55 +0700

    Update scm.py

--- Tools/scm.py
+++ Tools/scm.py
@@ -253,7 +253,7 @@ with col[1]:
                                 if not os.path.exists(folder):
                                     os.makedirs(folder)
                                 for kat in db_ia['KATEGORI'].unique():
-                                    if kat in ['Raw Material', 'Packaging']:
+                                    if kat in ['Raw Material']:
                                         df_ia = df_4217[(df_4217['Kategori Barang'].isin(db_ia[db_ia['KATEGORI']==kat]['FILTER'])) 
                                                         & ~(df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']=='Consume']['FILTER']))
                                                         & (df_4217['Cabang']==cab)] 
@@ -261,6 +261,21 @@ with col[1]:
                                         df_ia = df_ia.rename(columns={'Kode Barang':'Kode','Satuan':'Unit','DEVIASI(Rumus)':'Kuantitas','Nama Cabang':'Gudang'}).loc[:,['Nama Barang','Kode','Unit','Kuantitas','Biaya Satuan','Gudang','Tipe Penyesuaian','Nama Dept Barang','No Proyek Barang','Keterangan','No. Seri/Produksi Aktif','Merupakan No. Produksi','Nomor Unik/Produksi','Kuantitas Nomor Produksi']]
                                         if not df_ia.empty:
                                             df_ia.to_excel(f'{folder}/{kat}_{cab}_{nama_file}.xlsx', index=False)
+                                    if kat in ['Packaging']:
+                                        df_ia = df_4217[(df_4217['Kategori Barang'].isin(db_ia[db_ia['KATEGORI']==kat]['FILTER'])) 
+                                                        & ~(df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']=='Consume']['FILTER']))
+                                                        & (df_4217['Cabang']==cab) & ~(df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']=='Packaging Usage']['FILTER']))] 
+                                        df_ia[['Biaya Satuan','Nama Dept Barang','No Proyek Barang','Keterangan','No. Seri/Produksi Aktif','Merupakan No. Produksi','Nomor Unik/Produksi','Kuantitas Nomor Produksi']] = '' 
+                                        df_ia = df_ia.rename(columns={'Kode Barang':'Kode','Satuan':'Unit','DEVIASI(Rumus)':'Kuantitas','Nama Cabang':'Gudang'}).loc[:,['Nama Barang','Kode','Unit','Kuantitas','Biaya Satuan','Gudang','Tipe Penyesuaian','Nama Dept Barang','No Proyek Barang','Keterangan','No. Seri/Produksi Aktif','Merupakan No. Produksi','Nomor Unik/Produksi','Kuantitas Nomor Produksi']]
+                                        if not df_ia.empty:
+                                            df_ia.to_excel(f'{folder}/{kat} Deviasi_{cab}_{nama_file}.xlsx', index=False)
+                                        df_ia = df_4217[(df_4217['Kategori Barang'].isin(db_ia[db_ia['KATEGORI']==kat]['FILTER'])) 
+                                                        & ~(df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']=='Consume']['FILTER']))
+                                                        & (df_4217['Cabang']==cab) & (df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']=='Packaging Usage']['FILTER']))] 
+                                        df_ia[['Biaya Satuan','Nama Dept Barang','No Proyek Barang','Keterangan','No. Seri/Produksi Aktif','Merupakan No. Produksi','Nomor Unik/Produksi','Kuantitas Nomor Produksi']] = '' 
+                                        df_ia = df_ia.rename(columns={'Kode Barang':'Kode','Satuan':'Unit','DEVIASI(Rumus)':'Kuantitas','Nama Cabang':'Gudang'}).loc[:,['Nama Barang','Kode','Unit','Kuantitas','Biaya Satuan','Gudang','Tipe Penyesuaian','Nama Dept Barang','No Proyek Barang','Keterangan','No. Seri/Produksi Aktif','Merupakan No. Produksi','Nomor Unik/Produksi','Kuantitas Nomor Produksi']]
+                                        if not df_ia.empty:
+                                            df_ia.to_excel(f'{folder}/{kat} Usage_{cab}_{nama_file}.xlsx', index=False)
                                     if kat in ['Consume']:
                                         df_ia = df_4217[(df_4217['Nama Barang'].isin(db_ia[db_ia['KATEGORI']==kat]['FILTER']))
                                                         & (df_4217['Cabang']==cab)] 
