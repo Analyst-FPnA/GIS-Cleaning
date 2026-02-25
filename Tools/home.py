36bb02d5bb31c06fa3596b22485ce8baada57677
Analyst-FPnA <analyst.fpna@gmail.com>
2026-02-25 13:31:53 +0700

    Update home.py

--- Tools/home.py
+++ Tools/home.py
@@ -17,7 +17,7 @@ col = st.columns([4,1])
 with col[0]:
     st.image(image)
 with col[1]:
-    with st.expander("Latest Version Update Details: v2.1.21"):
+    with st.expander("Latest Version Update Details: v2.1.22"):
 
         success_html = """
         <div style="
@@ -29,7 +29,7 @@ with col[1]:
             border: 1px solid #c3e6cb;
         ">
         <ul style="padding-top: 10px; padding-bottom: 10px; padding-left: 12px; padding-right: 10px; margin: 0;">
-            <li>Update Database IA file on REKAP PENYESUAIAN INPUTAN IA [SCM-Processing]</li>
+            <li>Update on REKAP PENYESUAIAN INPUTAN IA [SCM-Processing]</li>
         </ul>
         </div>
         """
