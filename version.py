d95d2ca9ae6e7ffaa52a999d99ec01a8e97b3b5d
Analyst-FPnA <analyst.fpna@gmail.com>
2026-02-27 08:48:09 +0700

    Update version.py

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-version = "v2.1.22"
+version = "v2.1.23"
 data = "18/02/2026"
 detail = """
         <div style="
@@ -11,10 +11,10 @@ detail = """
             padding: 10px 12px;
         ">
         <div style="font-size: 13px; font-weight: bold; margin-bottom: 8px;">
-            New Version Update Details: v2.1.22
+            New Version Update Details: v2.1.23
         </div>
         <ul style="padding-left: 18px; margin: 0;">
-            <li>Update on REKAP PENYESUAIAN INPUTAN IA [SCM-Processing]</li>
+            <li>Convert values to negative for adjustment type "Pengurangan" on 41.01 [GIS-Processing]</li>
         </ul>
         </div>
         """
