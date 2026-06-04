c1c7fd8e95bb11c92ceb3d37d3012f6afd126da0
Analyst-FPnA <analyst.fpna@gmail.com>
2026-06-04 12:43:45 +0700

    Update version.py

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-version = "v2.1.40"
+version = "v2.1.41"
 data = "03/06/2026"
 detail = """
         <div style="
@@ -11,7 +11,7 @@ detail = """
             padding: 10px 12px;
         ">
         <div style="font-size: 13px; font-weight: bold; margin-bottom: 8px;">
-            New Version Update Details: v2.1.40
+            New Version Update Details: v2.1.41
         </div>
         <ul style="padding-left: 18px; margin: 0;">
             <li></li>
