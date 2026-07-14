1fe12b7a04ae4a5d8d3c9096d7a99c2a1e63e9d4
Analyst-FPnA <analyst.fpna@gmail.com>
2026-07-14 09:36:46 +0700

    Update scm.py

--- Tools/scm.py
+++ Tools/scm.py
@@ -923,15 +923,18 @@ with col[1]:
                             df_esb = pd.concat(df_esb, ignore_index=True)
                             df_esb = df_esb[['Branch','Sales Date','Menu Name','Menu Code','Qty']].assign(**{'Menu Code': df_esb['Menu Code'].astype(str)})
                             
-                            db_new_kode = pd.concat([pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx",sheet_name='RESTO')[['Branch','START COVER']].merge(
-                                pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx"), how='cross'),
-                                pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx", sheet_name='KODE_RESTO')], ignore_index=True)
-                            
+                            db_new_kode = pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx",sheet_name='RESTO')[['Branch','START COVER']].merge(
+                                pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx"), how='cross'
+                            )
                             db_new_kode.loc[:,['Menu Code','Menu Code New']] = db_new_kode[['Menu Code','Menu Code New']].astype(str).values
                             db_new_kode['START COVER'] = pd.to_datetime(db_new_kode['START COVER'])
+                            db_exclude_kode = pd.read_excel("Master/KODE DAN RESTO BOM NEW.xlsx", sheet_name="EXCLUDE")
+                            db_exclude_kode['EXCLUDE_DATE'] = pd.to_datetime(db_exclude_kode['EXCLUDE_DATE'])
                             df_esb['Sales Date'] = pd.to_datetime(df_esb['Sales Date'])
                             df_esb = df_esb.merge(db_new_kode, on=['Branch','Menu Code'], how='left')
                             df_esb.loc[df_esb['Sales Date']<df_esb['START COVER'], 'Menu Code New'] = np.nan
+                            df_esb = df_esb.merge(db_exclude_kode, left_on=['Branch','Sales Date'], right_on=['Branch','EXCLUDE_DATE'], how='left')
+                            df_esb.loc[df_esb['Sales Date']==df_esb['EXCLUDE_DATE'], 'Menu Code New'] = np.nan
                             
                             df_esb['Menu Code'] = df_esb['Menu Code New'].fillna(df_esb['Menu Code'])
                             df_esb = df_esb.drop(columns=['Menu Code New']).merge(df_4121[['Kode Barang Grup Barang','Kode Barang','Kuantitas']].rename(columns={'Kode Barang Grup Barang':'Menu Code'}), on='Menu Code', how='left')
